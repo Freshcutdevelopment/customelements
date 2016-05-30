a57e269ed0c1113edd0b16684f67d8074b1026a1
Sean Hunter <sean.hunter.aus@gmail.com>
2016-05-30 10:44:34 +0100

    added styling to custom io element

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-var XSeanProto = Object.create(HTMLElement.prototype);
+var UserBioProto = Object.create(HTMLElement.prototype);
 
-XSeanProto.createdCallback = function(){
+UserBioProto.createdCallback = function(){
     
-    var shadow = this.createShadowRoot();
-    
-    var heading = document.createElement('h1');
-    heading.innerHTML = "sean";
-    shadow.appendChild(heading);
+    var root = this.createShadowRoot();
+    root.innerHTML = '<style>' + 
+                     ' div { ' + 
+                     '   box-shadow: 0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12); ' +
+                     '   width:300px;' +
+                     '   padding:10px;' +
+                     '}'+
+                     ' img { ' + 
+                     '   width:30px;' +
+                     '   height:30px;' +
+                     '   float:left;' +
+                     '   text-align:left;' +
+                     '   margin-top:10px;' +
+                     '   margin-right:10px;' +
+                     '}'+
+                     ' p { ' + 
+                     '   padding-bottom:0px;' +
+                     '}'+
+                     '</style>' + 
+                 '<div><img src="img/sauron.jpg"></img> <p> Sauron - The Necromancer </p></div>';
     
 };
 
-var xSean = document.registerElement('x-sean',{
-  prototype:XSeanProto  
+var userBio = document.registerElement('user-bio',{
+  prototype:UserBioProto  
 });
